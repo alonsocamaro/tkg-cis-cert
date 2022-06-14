661ddfffa548bd42ec145fe261bcdb76c5d7538c
alonsocamaro <u.alonsocamaro@f5.com>
2022-06-14 13:12:24 +0200

    Update install-helm.sh

--- install-helm.sh
+++ install-helm.sh
@@ -1,3 +1,3 @@
-kubectl create secret generic bigip-login -n kube-system --from-literal=username=admin --from-literal=$1
+kubectl create secret generic bigip-login -n kube-system --from-literal=username=admin --from-literal=password=$1
 helm repo add f5-stable https://f5networks.github.io/charts/stable
 helm install -f values.yaml cis f5-stable/f5-bigip-ctlr
