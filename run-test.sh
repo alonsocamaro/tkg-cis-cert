202c82976d22584dcba55aab0b2c32ef18bc486f
alonsocamaro <u.alonsocamaro@f5.com>
2022-06-16 10:43:27 +0200

    Update run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -3,8 +3,8 @@ kubectl apply -f service.yaml
 kubectl apply -f ingress.yaml
 sleep 60
 
-while true ; do
+while sleep 5 ; do
   date
-  curl --max-time 2 192.168.100.151
+  curl --max-time 2 -o /dev/null -w "%{http_code}" 192.168.100.151
 done
 
